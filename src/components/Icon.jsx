bd7c8b6342cdd73519bf9176c9233f4fea0f9367
akshith <akshithmnaik2005@gmail.com>
2025-07-28 00:31:25 +0530

    Initial commit

---
+++ src/components/Icon.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+const Icon = () => {
+  return (
+    <div className='flex items-center justify-center mt-5 '>
+        <img src="/todo-list.webp" alt="iTask Icon" className='w-10 h-10'/>
+        <span className="title text-4xl font-bold">iTask</span>
+    </div>
+  )
+}
+
+export default Icon
